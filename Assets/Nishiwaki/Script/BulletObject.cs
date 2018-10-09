16cd2cfbea6f09c0435f1059d7093b889c0ace78
DESKTOP-28F38VP\user <nkc.daiki.nishiwaki@gmail.com>
2018-10-09 14:01:56 +0900

    弾が出た

---
+++ Assets/Nishiwaki/Script/BulletObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoguLike.Nishiwaki.Bullet;
+
+namespace RoguLike.Nishiwaki.Bullet
+{
+    public class BulletObject : BulletBase
+    {
+        // Use this for initialization
+        void Start()
+        {
+            // BulletParameterから
+            bulletPara = new BulletParameter(1, 1);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Rigidbody rig = GetComponent<Rigidbody>();
+
+            Vector3 now = rig.position;
+            // 銃に依存?
+            now += new Vector3(1.0f, 0.0f, 0.0f);
+
+            rig.position = now;
+            //rig.position += Vector3.forward * bulletPara.Speed;
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            // 敵に当たったら
+            if (other.gameObject.tag == "Enemy")
+            {
+                Debug.Log("HIT");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
