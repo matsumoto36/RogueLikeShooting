fa308afdeeeafeabff5e3e594bc707f4418596e8
DESKTOP-28F38VP\user <nkc.keita.takahasi@gmail.com>
2018-12-20 15:59:14 +0900

    effectのデータをtakahashiフォルダーに統合

---
+++ Assets/takahashi/Script/obj_height_c.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_height_c : MonoBehaviour {
+
+    public string obj_name;
+    public GameObject m_obj;
+
+
+	// Use this for initialization
+	void Start () {
+        m_obj = GameObject.Find(obj_name);
+        //m_obj = transform.root.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float f = m_obj.GetComponent<Renderer>().material.GetFloat("_v_jouge");
+        gameObject.GetComponent<Renderer>().material.SetFloat("_v_jouge",f);
+        gameObject.GetComponent<Renderer>().material.SetFloat("_takasa",m_obj.GetComponent<obj_height>().objy);
+        gameObject.GetComponent<Renderer>().material.SetColor("_emi_color", m_obj.GetComponent<obj_height>().c_emistion);
+    }
+}
