dd9d2f6cd650c4ea4a8d5a34ce5d2f05f929a257
Reqweldzen <nanashiro2015@gmail.com>
2019-01-11 09:16:44 +0900

    update rider bridge plugin

--- Assets/Takahashi/Script/DissolveShaderControl.cs
+++ Assets/Takahashi/Script/DissolveShaderControl.cs
@@ -1,24 +1,25 @@
 ﻿using System;
+using System.Threading;
+using UniRx;
+using UniRx.Async;
+using UniRx.Async.Triggers;
+using UniRx.Triggers;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace DDD.Takahashi
 {
 	public class DissolveShaderControl : MonoBehaviour
 	{
-		private static readonly int VJouge = Shader.PropertyToID("_v_jouge");
+		private static readonly int Border = Shader.PropertyToID("_v_jouge");
 		private static readonly int Emission = Shader.PropertyToID("_emi_color");
 		private static readonly int Height = Shader.PropertyToID("_takasa");
 		
 		private Transform _transformCache;
 		private Renderer _renderer;
-		private ShaderMode _shaderMode;
 		private float _startValue;
 		
 		public bool UpperOnAwake;
-		public Color EmissionColor;
-
-		
+		public ColorReactiveProperty EmissionColor;		
 		
 		[Header("Emit Parameters")]
 		public float UpperHeight;
@@ -26,81 +27,83 @@ namespace DDD.Takahashi
 		
 		
 		public float Speed;
-		
-		public float PositionY { get; private set; }
-		
 
-		// Use this for initialization
-		private void Start()
+		public float PositionY => _transformCache.position.y;
+
+		private CancellationToken _cancellationToken;
+
+		private void Awake()
 		{
 			_transformCache = transform;
 			_renderer = GetComponent<Renderer>();
-			
-			if (UpperOnAwake)
-			{
-				_renderer.material.SetFloat(VJouge, UpperHeight);
-				_startValue = UpperHeight;
-				
-			}
-			else
-			{
-				_renderer.material.SetFloat(VJouge, LowerHeight);
-				_startValue = LowerHeight;
-			}
 		}
-
-		// Update is called once per frame
-		private void Update()
+		
+		// Use this for initialization
+		private void Start()
 		{
-			PositionY = _transformCache.position.y;
-			_renderer.material.SetFloat(Height, PositionY);
-			_renderer.material.SetColor(Emission, EmissionColor);
-			SpawnManager();
+			_cancellationToken = this.GetCancellationTokenOnDestroy();
+			
+			_renderer.material.SetFloat(Border, UpperOnAwake ? UpperHeight : LowerHeight);
+
+			this.UpdateAsObservable().Subscribe(_ => _renderer.material.SetFloat(Height, PositionY));
+			EmissionColor.Subscribe(color => _renderer.material.SetColor(Emission, color)).AddTo(this);
 		}
 
+		private readonly Subject<(DissolveMode, float)> _dissolveSubject = new Subject<(DissolveMode, float)>();
+		public IObservable<(DissolveMode, float)> OnDissolve => _dissolveSubject;
+		
 		public void Show()
 		{
-			_shaderMode = ShaderMode.Composite;
+			async UniTaskVoid CompositeAsync()
+			{
+				var delta = LowerHeight;
+				_renderer.material.SetFloat(Border, delta);
+				
+				while (!_cancellationToken.IsCancellationRequested)
+				{
+					delta += Speed * Time.deltaTime;
+					_renderer.material.SetFloat(Border, delta);
+						
+					_dissolveSubject.OnNext((DissolveMode.Composite, delta));
+					
+					if (UpperHeight <= delta)
+					{
+						_startValue = UpperHeight;
+						break;
+					}
+
+					await UniTask.Yield();
+				}
+			}
+			
 			_startValue = LowerHeight;
+
+			CompositeAsync().Forget();
 		}
 
 		public void Hide()
 		{
-			_shaderMode = ShaderMode.Dissolve;
-			_startValue = UpperHeight;
-		}
-
-		private void SpawnManager()
-		{
-			switch (_shaderMode)
+			async UniTaskVoid DissolveAsync()
 			{
-				case ShaderMode.Composite:
+				while (!_cancellationToken.IsCancellationRequested)
 				{
-					_startValue += Speed;
-					_renderer.material.SetFloat(VJouge, _startValue);
-					if (UpperHeight <= _startValue)
-					{
-						_shaderMode = ShaderMode.Neutral;
-						_startValue = UpperHeight;
-					}
-					break;
-				}
-				case ShaderMode.Dissolve:
-				{
-					
 					_startValue -= Speed;
-					_renderer.material.SetFloat(VJouge, _startValue);
-					if (LowerHeight >=_startValue)
+					_renderer.material.SetFloat(Border, _startValue);
+					if (LowerHeight >= _startValue)
 					{
-						_shaderMode = ShaderMode.Neutral;
 						_startValue = LowerHeight;
 					}
-					break;
+
+					await UniTask.Yield();
 				}
 			}
+			
+			_startValue = UpperHeight;
+
+			DissolveAsync().Forget();
 		}
 
-		private enum ShaderMode
+		public enum DissolveMode
 		{
 			Neutral = 0,
 			/// <summary>
