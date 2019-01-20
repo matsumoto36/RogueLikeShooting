bf7c6344ea0be4d82bb55369f06f0cfff601b211
RyoMatsumoto <nkc.ryo.matsumoto@gmail.com>
2019-01-21 04:26:46 +0900

    ミニマップ表示部分の作成

--- Assets/Matsumoto/Script/MiniMap/MiniMapViewer.cs
+++ Assets/Matsumoto/Script/MiniMap/MiniMapViewer.cs
@@ -39,7 +39,11 @@ namespace DDD.Matsumoto.Minimap {
 
 			//イベント登録
 			//_miniMapSystem.OnMapChanged += Draw;
-			_miniMapSystem.OnStarted += SetMapFloorImage;
+			_miniMapSystem.OnStarted += () => {
+				SetMapFloorImage();
+				StairIcon.transform.SetAsLastSibling();
+				PlayerIcon.transform.SetAsLastSibling();
+			};
 
 		}
 
@@ -51,35 +55,31 @@ namespace DDD.Matsumoto.Minimap {
 			_mapSize = _miniMapSystem.MapSize;
 			_mapImages = new Image[_mapSize.Y, _mapSize.X];
 			_imagePositions = new Vector2[_mapSize.Y, _mapSize.X];
+			var delta = (RoomSize + AisleLength) * 0.5f;
 
 			Vector2 position = new Vector2();
 			for (int i = 0; i < _mapSize.Y; i++) {
 				for (int j = 0; j < _mapSize.X; j++) {
-					//Todo 加算の仕方がおかしい気がする
-					if (i % 2 != 0 && j % 2 != 0) {
-						position.x += RoomSize;
+
+					//床も廊下もない空間
+					if(i % 2 != 0 && j % 2 != 0) {
+						position.x += delta;
 						continue;
 					}
+
 
 					var image = Instantiate(FloorImagePre, MapParent);
 
 					//両方偶数の時は床
 					if(i % 2 == 0 && j % 2 == 0) {
-
 						image.rectTransform.sizeDelta = new Vector2(RoomSize, RoomSize);
-
-						image.rectTransform.anchoredPosition = position;
-						_imagePositions[i, j] = position;
-						_mapImages[i, j] = image;
-
-						position.x += RoomSize;
 					}
 					//そのほかは廊下
 					else {
 
 						Vector2 aisleSize;
-						//縦方向の接続
-						if (i % 2 == 0) {
+						//横方向の接続
+						if(i % 2 == 0) {
 							aisleSize = new Vector2(AisleLength, AisleWidth);
 						}
 						else {
@@ -87,24 +87,20 @@ namespace DDD.Matsumoto.Minimap {
 						}
 
 						image.rectTransform.sizeDelta = aisleSize;
-
-						image.rectTransform.anchoredPosition = position;
-						_imagePositions[i, j] = position;
-						_mapImages[i, j] = image;
-
-						position.x += aisleSize.x;
 					}
+
+					image.rectTransform.anchoredPosition = position;
+					image.color = MiniMapColor;
+					_imagePositions[i, j] = position;
+					_mapImages[i, j] = image;
+
+					position.x += delta;
+
 				}
 
 				position.x = 0.0f;
+				position.y += delta;
 
-				//Yの加算
-				if (i % 2 == 0) {
-					position.y += RoomSize;
-				}
-				else {
-					position.y += AisleLength;
-				}
 			}
 
 		}
