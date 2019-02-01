17991221677e6f57137293b027caf9f89d2ca055
RyoMatsumoto <nkc.ryo.matsumoto@gmail.com>
2019-02-01 10:33:33 +0900

    武器チェンジUI追加、フォント追加

---
+++ Assets/Matsumoto/Script/UI/UIWeaponChange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DDD.Matsumoto.UI {
+
+	/// <summary>
+	/// 武器変更時のUI
+	/// </summary>
+	public class UIWeaponChange : UIBase {
+
+		public Image FillImage;
+		public GameObject ControllerButtonImage;
+		public GameObject KeyboardButtonImage;
+
+		public void Start() {
+
+			//初期設定
+			SetControllerType(false);
+			SetAmount(0);
+
+		}
+
+		/// <summary>
+		/// 乗っ取り状態を設定
+		/// </summary>
+		/// <param name="amount"></param>
+		public void SetAmount(float amount) {
+			FillImage.fillAmount = amount;
+		}
+
+		/// <summary>
+		/// ボタンの画像タイプを設定
+		/// </summary>
+		/// <param name="isKeyboard"></param>
+		public void SetControllerType(bool isKeyboard) {
+			ControllerButtonImage.SetActive(!isKeyboard);
+			KeyboardButtonImage.SetActive(isKeyboard);
+		}
+	}
+}
+
