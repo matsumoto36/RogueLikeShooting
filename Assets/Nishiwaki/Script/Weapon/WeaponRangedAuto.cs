af160ab2b5c4c0aa8fa7dccae1f8bf203aee4c29
Reqweldzen <nanashiro2015@gmail.com>
2019-02-03 08:50:07 +0900

    野良Weaponがちゃんと消える

--- Assets/Nishiwaki/Script/Weapon/WeaponRangedAuto.cs
+++ Assets/Nishiwaki/Script/Weapon/WeaponRangedAuto.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections;
+using System.Threading;
+using DDD.Katano;
 using DDD.Katano.View;
 using DDD.Matsumoto;
+using UniRx;
 using UniRx.Async;
+using UniRx.Async.Triggers;
+using Unity.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -11,14 +16,20 @@ namespace DDD.Nishiwaki.Item
 	public class WeaponRangedAuto : WeaponRanged
 	{
 		private const float EnemyWeaponRateLimit = 2f;
-		private WeaponBullet.Pool _weaponPool;
+		private WeaponBullet.Pool _bulletPool;
+		private CancellationToken _token;
 		
 		public bool CanShot { get; private set; } = true;
 
 		[Inject]
-		private void Construct(WeaponBullet.Pool weaponPool)
+		private void Construct(WeaponBullet.Pool bulletPool)
 		{
-			_weaponPool = weaponPool;
+			_token = this.GetCancellationTokenOnDestroy();
+			_bulletPool = bulletPool;
+
+			MessageReceiver.Receive<MazeSignal.FloorDestruct>()
+				.Subscribe(_ => gameObject.Destroy())
+				.AddTo(this);
 		}
 		
 		public override void Attack()
@@ -27,21 +38,21 @@ namespace DDD.Nishiwaki.Item
 			CanShot = false;
 			// 弾の発射位置
 			
-			CooldownAsync().Forget();
+			CooldownAsync(_token).Forget();
 			
 			Debug.Log("Attack()");
 			
 			// iBullet.BulletCreate(transform);
 
-			_weaponPool.Spawn(characterCore, transform);
+			_bulletPool.Spawn(characterCore, transform);
 
 			//ショット音
 			Matsumoto.Audio.AudioManager.PlaySE("se_maoudamashii_system42");
 		}
 
-		private async UniTaskVoid CooldownAsync()
+		private async UniTaskVoid CooldownAsync(CancellationToken token = default)
 		{
-			await UniTask.Delay(TimeSpan.FromSeconds(characterCore.Alliance == CharacterType.Enemy ? WeaponRangedPara.WaitTime * EnemyWeaponRateLimit : WeaponRangedPara.WaitTime));
+			await UniTask.Delay(TimeSpan.FromSeconds(characterCore.Alliance == CharacterType.Enemy ? WeaponRangedPara.WaitTime * EnemyWeaponRateLimit : WeaponRangedPara.WaitTime), cancellationToken: token);
 			CanShot = true;
 		}
 
