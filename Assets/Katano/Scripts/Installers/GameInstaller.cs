d6be5e42fa0bf9fb789f89fed4a550e322db3a66
Reqweldzen <nanashiro2015@gmail.com>
2019-02-03 03:41:21 +0900

    Stage replaced

--- Assets/Katano/Scripts/Installers/GameInstaller.cs
+++ Assets/Katano/Scripts/Installers/GameInstaller.cs
@@ -1,46 +1,18 @@
-using DDD.Katano.Maze;
-using DDD.Katano.Model;
-using DDD.Katano.View.RoomComponents;
-using DDD.Matsumoto;
-using DDD.Matsumoto.Character;
-using DDD.Matsumoto.Character.Asset;
 using UniRx;
-using UnityEngine;
 using Zenject;
 
 namespace DDD.Katano.Installers
 {
 	/// <summary>
-	/// ゲームシーンインストーラ
+	///     ゲームシーンインストーラ
 	/// </summary>
 	public class GameInstaller : MonoInstaller<GameInstaller>
 	{
-		public GameObject PlayerSpawner;
-		public GameObject PlayerPrefab;
-		
 		public override void InstallBindings()
 		{
-			Container.Bind<PlayerHealthProvider>().FromSubContainerResolve().ByMethod(InstallPlayerHealthProvider).AsSingle();
-			
 			Container.Bind<MessageBroker>().AsSingle();
 			Container.Bind<IMessageReceiver>().To<MessageBroker>().FromResolve();
 			Container.Bind<IMessagePublisher>().To<MessageBroker>().FromResolve();
-
-			Container.Bind<MazeViewBuilder.Factory>().AsSingle();
-			Container.Bind<PlayerBuilder>().AsSingle().WithArguments(PlayerPrefab);
-			Container.Bind<EnemyBuilder>().AsSingle().WithArguments(PlayerPrefab);
-			
-			
-			Container.BindInstance(PlayerSpawner).WhenInjectedInto<PlayerRoomComponent>();
-
-			Container.Bind<PlayerSpawnerFactory>().AsSingle().WithArguments(PlayerSpawner);			
-		}
-
-		private void InstallPlayerHealthProvider(DiContainer container)
-		{
-			var playerAsset = Container.Resolve<PlayerAsset>();
-
-			container.Bind<PlayerHealthProvider>().AsSingle().WithArguments(playerAsset.HP);
 		}
 	}
 }
